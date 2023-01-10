cf5257f1f2f360169cd5e7680b3d6f26bdabdb49
Каримов Руслан Маратович <yoursluzhba@gmail.com>
2023-01-10 22:41:14 +0300

    Updates

---
+++ precache-manifest.9a627e0d404d0b456a6c9cdbcd205082.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6dfb344449c1d552b2899fab6161c7f",
+    "url": "/react-project-1.ts/index.html"
+  },
+  {
+    "revision": "888d23cbd8a1e9c94106",
+    "url": "/react-project-1.ts/static/css/main.3c6e3a2d.chunk.css"
+  },
+  {
+    "revision": "3e732233062ca87af1ba",
+    "url": "/react-project-1.ts/static/js/2.932ee09d.chunk.js"
+  },
+  {
+    "revision": "888d23cbd8a1e9c94106",
+    "url": "/react-project-1.ts/static/js/main.e65c75b1.chunk.js"
+  },
+  {
+    "revision": "15ba8da022326a37e42a",
+    "url": "/react-project-1.ts/static/js/runtime~main.96d5e0fc.js"
+  }
+]);
